6b6e0f1048aeb2d6749b3db7630f0bc47f41d3fc
Renan Miguel <renan@rdmss.com.br>
2022-08-03 21:26:35 -0300

    Add Classe Conversora

---
+++ src/main/java/br/com/pacetech/pacetech/git/Conversor.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.pacetech.pacetech.git;
+
+/**
+ *
+ * @author Renan Miguel
+ */
+public class Conversor {
+    
+    //esta é a classe conversora
+    
+}
